6fbdc43c994e49a05487136d4fbc436cea7c17c5
Rahul Mitra <rmitra@avinetworks.com>
2017-12-18 23:44:08 +0000

    Golang SDK: Add support for token based authentication AviSession; API versioning fixed to AviSession; Bug fixes

--- go/session/session_test.go
+++ go/session/session_test.go
@@ -2,29 +2,45 @@ package session
 
 import (
 	"github.com/avinetworks/sdk/go/models"
-	"log"
 	"reflect"
 	"testing"
+	"github.com/golang/glog"
 )
 
-func TestAviSession(t *testing.T) {
-	avisess, err := NewAviSession("10.10.25.201", "admin", SetPassword("avi123"), SetInsecure)
+func getSessions(t *testing.T) (
+	credentialsSession *AviSession, authTokenSession *AviSession) {
+	/* Test username/password authentication */
+	credentialsSession, err := NewAviSession("10.10.25.201",
+		"admin", SetPassword("avi123"), SetInsecure)
 	if err != nil {
-		t.Error("Session Creation failed: ", err)
-		return
+		t.Fatalf("Session Creation failed: %s", err)
 	}
+
+	/* Test token authentication */
+	authTokenSession, err = NewAviSession("localhost", "admin",
+		SetTokenAuth, SetInsecure)
+
+	if err != nil {
+		t.Fatalf("Session Creation failed: %s", err)
+	}
+
+	return credentialsSession, authTokenSession
+}
+
+func testAviSession(t *testing.T, avisess *AviSession) {
+
 	var res interface{}
-	err = avisess.Get("api/tenant", &res)
-	log.Println("res: ", res, " err:", err)
+	err := avisess.Get("api/tenant", &res)
+	glog.Infof("res: %s, err: %s", res, err)
 	resp := res.(map[string]interface{})
-	log.Println("count: ", resp["count"])
+	glog.Infof("count: %s", resp["count"])
 
 	// create a tenant
 	tenant := make(map[string]string)
 	tenant["name"] = "testtenant"
 	var tres interface{}
 	err = avisess.Post("api/tenant", &tenant, &tres)
-	log.Println("res: ", tres, " err:", err)
+	glog.Infof("res: %s, err: %s", tres, err)
 	if err != nil {
 		t.Error("Tenant Creation failed: ", err)
 		return
@@ -32,13 +48,13 @@ func TestAviSession(t *testing.T) {
 
 	// check tenant is created well
 	err = avisess.Get("api/tenant?name=testtenant", &res)
-	log.Println("res: ", res, " err:", err)
+	glog.Infof("res: %s, err: %s", res, err)
 	if reflect.TypeOf(res).Kind() == reflect.String {
 		t.Errorf("Got string instead of json!")
 		return
 	}
 	resp = res.(map[string]interface{})
-	log.Println("count: ", resp["count"])
+	glog.Infof("count: %s", resp["count"])
 	currCount := resp["count"].(float64)
 	if currCount != 1.0 {
 		t.Errorf("could not find a tenant with name testtenant")
@@ -48,7 +64,7 @@ func TestAviSession(t *testing.T) {
 
 	// delete the tenant
 	err = avisess.Delete("api/tenant/" + tenant["uuid"])
-	log.Println("err: ", err)
+	glog.Infof("err: %s", err)
 	if err != nil {
 		t.Error("Deletion failed")
 		return
@@ -57,44 +73,48 @@ func TestAviSession(t *testing.T) {
 	// check to make sure that the tenant is not there any more
 	// check tenant is created well
 	err = avisess.Get("api/tenant?name=testtenant", &res)
-	log.Println("res: ", res, " err:", err)
+	glog.Infof("res: %s, err: %s", res, err)
 	resp = res.(map[string]interface{})
-	log.Println("count: ", resp["count"])
-	curr_count = resp["count"].(float64)
-	if curr_count != 0.0 {
+	glog.Infof("count: ", resp["count"])
+	currCount = resp["count"].(float64)
+	if currCount != 0.0 {
 		t.Errorf("Expecting no tenant with that name")
 		return
 	}
 
-	//t.Error("Just to force output")
 }
 
-func TestAviPool(t *testing.T) {
-	avisess, err := NewAviSession("10.10.25.201", "admin", SetPassword("avi123"), SetInsecure)
-	if err != nil {
-		t.Error("Session Creation failed: ", err)
-		return
-	}
-
+func testAviPool(t *testing.T, avisess *AviSession) {
 	tpool := models.Pool{}
 	pname := "testpool"
 	tpool.Name = pname
 	var res models.Pool
-	err = avisess.Post("api/pool", tpool, &res)
-	log.Println("res: ", res, " err:", err)
+	err := avisess.Post("api/pool", tpool, &res)
+	glog.Infof("res: %s, err: %s", res, err)
 	if err != nil {
-		t.Error("Pool Creation failed: ", err)
+		t.Fatalf("Pool Creation failed: %s", err)
 	}
 
 	var npool2 models.Pool
 	err = avisess.GetObjectByName("pool", "testpool", &npool2)
 
-	log.Printf("npool: %+v err: %+v", npool2, err)
-	log.Println("name: ", npool2.Name)
+	glog.Infof("npool: %+v err: %+v", npool2, err)
+	glog.Infof("name %s: ", npool2.Name)
 
 	err = avisess.Delete("api/pool/" + npool2.UUID)
 	if err != nil {
-		t.Error("Pool deletion failed: ", err)
+		t.Fatalf("Pool deletion failed: %s", err)
 	}
-	//t.Error("Just to force output")
+}
+
+func TestAviSession(t *testing.T) {
+	credentialsSession, authTokenSession := getSessions(t)
+	testAviSession(t, credentialsSession)
+	testAviSession(t, authTokenSession)
+}
+
+func TestAviPool(t *testing.T) {
+	credentialsSession, authTokenSession := getSessions(t)
+	testAviPool(t, credentialsSession)
+	testAviPool(t, authTokenSession)
 }
